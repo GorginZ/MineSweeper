7931f2b48ae9f71939b322166e8bbb9f0d4b1490
GorginZ <georgia.z.leng@gmail.com>
2020-11-25 13:21:34 +1100

    squares are 'smart' and responsible for what is revealed and flagged. tests passing and some restructured functions in Minefield. Minefield can tell us how many mines are on it now

--- MineSweeper.Tests/MineFieldTests.cs
+++ MineSweeper.Tests/MineFieldTests.cs
@@ -30,15 +30,19 @@ namespace MineSweeper.Tests
       var field = new MineField(5, 5, minePlacement);
       Assert.Equal(25, field.MineCount);
     }
-    // [Fact]
-    // public void SquareHaveCorrectSquareHintValue()
-    // {
-    //   var minePlacement = new SetMinePositions(new HashSet<RowColumn> { new RowColumn(0, 0), new RowColumn(0, 1), new RowColumn(0, 2), new RowColumn(0, 3), new RowColumn(0, 4), new RowColumn(1, 0), new RowColumn(2, 0), new RowColumn(3, 0), new RowColumn(4, 0), new RowColumn(4, 1), new RowColumn(4, 2), new RowColumn(4, 3), new RowColumn(4, 4), new RowColumn(1, 4), new RowColumn(2, 4), new RowColumn(3, 4) });
-    //   var field = new MineField(5, 5, minePlacement);
-    //   Assert.Equal(5, field.Field[1, 1].SquareHintValue);
-    //   Assert.Equal(3, field.Field[1, 2].SquareHintValue);
-    //   Assert.Equal(5, field.Field[3, 1].SquareHintValue);
-    //   Assert.Equal(0, field.Field[2, 2].SquareHintValue);
-    // }
+    [Fact]
+    public void SquareHintValuesAreAccuratelyCalculated()
+    {
+      var minePositioning = new SetMinePositions(new HashSet<RowColumn> { new RowColumn(0, 0), new RowColumn(2, 2), new RowColumn(4, 4) });
+      var mineField = new MineField(5, 5, minePositioning);
+      var game = new Game(mineField);
+      var expectedField = " 1...\n"
+                        + " 211.\n"
+                        + "   1.\n"
+                        + "   21\n"
+                        + "     \n";
+      game.HandleSelectedSquare(new RowColumn(0, 4));
+      Assert.Equal(expectedField, game.GetCurrentField());
+    }
   }
 }
